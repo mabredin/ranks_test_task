fcc15acbdea81eff7705aa2f4e330bea0556e0f1
mabredin <mbredin@bk.ru>
2023-11-25 20:03:14 +0300

    Add links to item list page

--- src/item/urls.py
+++ src/item/urls.py
@@ -5,7 +5,7 @@ from item import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.ItemListView.as_view()),
+    path('', views.ItemListView.as_view(), name='item_list'),
     path('item/<int:pk>/', views.ItemDetailView.as_view(), name='item'),
     path('success/', views.page_success),
     path('cancel/', views.page_cancel),
